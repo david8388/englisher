696f7796a508a4f12b0000aebe3a05bc2561b7ec
David <hsienwu830808@gmail.com>
2020-08-01 20:20:03 +0800

    feat: inject bootstrap, and using navbar/appMain component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <navbar />
+    <app-main />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import Navbar from './layout/navbar';
+import AppMain from './layout/main/appMain';
 
 @Options({
   components: {
     HelloWorld,
+    Navbar,
+    AppMain
   },
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
