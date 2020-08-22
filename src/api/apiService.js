5c1ccb4f5f2239740e35b8b0641279a6bcb95bdd
David <hsienwu830808@gmail.com>
2020-08-22 15:55:27 +0800

    feat: add get words with filter API

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -8,5 +8,6 @@ const request = axios.create({
 })
 
 export const getWords = () => request.get('/word')
+export const getWordsWithFilter = (startDate, endDate) => request.get(`/word?startDate=${startDate}&endDate=${endDate}`)
 export const getRandomWords = () => request.get('/word/random')
 export const saveWord = data => request.post('/word', data)
